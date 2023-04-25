757117a216aaae90f3b11f94b2e1c613e89a8c04
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-26 00:06:15 +0800

    forgot to write else in if else

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -6,11 +6,17 @@ line_2_decision = input("Are there more lines? y/n: ")
 if line_2_decision == "y":
     # Enter line 2
     second_line = input("What is your Second line? : ")
-    # let user choose to write more lines
-    line_3_decision = input("Are there more lines? y/n: ")
-    if line_3_decision == "y":
-        # Enter line 3
-        third_line = input("What is your Third line? : ")
+else:
+    second_line = input(("Please continue, what is your second line? : "))
+
+# let user choose to write more lines
+line_3_decision = input("Are there more lines? y/n: ")
+if line_3_decision == "y":
+    # Enter line 3
+    third_line = input("What is your Third line? : ")
+else:
+    third_line = input(("Please continue, what is your third line? : "))
+
 # enter all line to mylife.txt
 with open("mylife.txt", "w") as my_life_line:
     my_life_line.write(starting_line + "\n" + second_line + "\n" + third_line + "\n")
