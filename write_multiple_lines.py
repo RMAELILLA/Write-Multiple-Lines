11e0af73eceabaa4f37c1ecc038e91d80ad14556
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-26 00:11:58 +0800

    successful code

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -17,7 +17,13 @@ if line_3_decision == "y":
 else:
     third_line = input(("Please continue, what is your third line? : "))
 
-input("Are there more lines? y/n: ")
+closing_statement = input("Are there more lines? y/n: ")
+if closing_statement == "y":
+    print("Sorry this code is for 3 lines only, thank you for your participation.")
+elif closing_statement == "n":
+    print("Thank you for participation!")
+else:
+    print("I don't recognize your input, please input y or n only, thank you.")
 # enter all line to mylife.txt
 with open("mylife.txt", "w") as my_life_line:
     my_life_line.write(starting_line + "\n" + second_line + "\n" + third_line + "\n")
