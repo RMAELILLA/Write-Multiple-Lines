7d5f396d39ce02b5cc575b651e18ed5efeb0e629
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-25 23:41:22 +0800

    successfully write first and 2nd line

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -1,8 +1,6 @@
 # Let user input line
 starting_line = input("Good Day! input your this program will let you input lines. Please input your first line: ")
 # Enter line 1 to mylife.txt
-# with open("mylife.txt", "w") as my_life_line:
-#    my_life_line.write(starting_line)
 # let user choose to write more lines
 line_2_decision = input("Are there more lines? y/n: ")
 if line_2_decision == "y":
