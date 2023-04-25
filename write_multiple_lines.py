ba86892e3bdd14e6e0959d28dd9529949af307d6
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-25 23:11:01 +0800

    let user input line 1

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -1,4 +1,5 @@
 # Let user input line
+starting_line = input("Good Day! input your this program will let you input lines. Please input your first line: ")
 # Enter line 1
 # let user choose to write more lines
 # Enter line 2
