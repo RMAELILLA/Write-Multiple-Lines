8da26719e814bc4c0352ee2a6edd0018b30a110d
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-25 23:20:09 +0800

    enter line 1 to mylife.txt to test writing in txt file

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -1,6 +1,8 @@
 # Let user input line
 starting_line = input("Good Day! input your this program will let you input lines. Please input your first line: ")
-# Enter line 1
+# Enter line 1 to mylife.txt
+with open("mylife.txt", "w") as my_life_line:
+    my_life_line.write(starting_line)
 # let user choose to write more lines
 # Enter line 2
 # let user choose to write more lines
