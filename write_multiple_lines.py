e8acae5f46a25d4101b7c96b8da570b592ea36b6
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-25 23:37:57 +0800

    failed to write 2nd line

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -1,8 +1,8 @@
 # Let user input line
 starting_line = input("Good Day! input your this program will let you input lines. Please input your first line: ")
 # Enter line 1 to mylife.txt
-with open("mylife.txt", "w") as my_life_line:
-    my_life_line.write(starting_line)
+# with open("mylife.txt", "w") as my_life_line:
+#    my_life_line.write(starting_line)
 # let user choose to write more lines
 line_2_decision = input("Are there more lines? y/n: ")
 if line_2_decision == "y":
@@ -11,3 +11,5 @@ if line_2_decision == "y":
     # let user choose to write more lines
     # Enter line 3
     # enter all line to mylife.txt
+with open("mylife.txt", "w") as my_life_line:
+    my_life_line.write(starting_line, secondline_line + "\n")
