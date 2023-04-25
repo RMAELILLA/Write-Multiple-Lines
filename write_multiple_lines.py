c38cbe6d12c0873bacbc1427def1bfa5bd08d9e7
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-25 23:26:59 +0800

    user input for line 2

--- write_multiple_lines.py
+++ write_multiple_lines.py
@@ -5,7 +5,9 @@ with open("mylife.txt", "w") as my_life_line:
     my_life_line.write(starting_line)
 # let user choose to write more lines
 line_2_decision = input("Are there more lines? y/n: ")
-# Enter line 2
-# let user choose to write more lines
-# Enter line 3
-# enter all line to mylife.txt
+if line_2_decision == "y":
+    # Enter line 2
+    input("What is your Second line? : ")
+    # let user choose to write more lines
+    # Enter line 3
+    # enter all line to mylife.txt
